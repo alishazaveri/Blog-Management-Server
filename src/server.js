1d60eb5de56ad5dec640efb3a7cd5b133d34ba7e
Alisha Zaveri <alisha.zaveri2@gmail.com>
2024-09-26 13:43:44 +0530

    Server setup

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+config();
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started at port ${process.env.PORT}`);
+});
