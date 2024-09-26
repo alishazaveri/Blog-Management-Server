06aedef42d30bff81d097134104ccfa7c299b61c
Alisha Zaveri <alisha.zaveri2@gmail.com>
2024-09-27 02:40:37 +0530

    Add Frontend Url in Cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://blog-mg.netlify.app/",
     credentials: true,
   })
 );
